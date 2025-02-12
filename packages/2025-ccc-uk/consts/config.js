c3f992b4113dd818108b2bbc2d2be1ac50521811
YiHsuan <v61265@gmail.com>
2025-02-13 02:21:14 +0800

    refactor: update wording

--- packages/2025-ccc-uk/consts/config.js
+++ packages/2025-ccc-uk/consts/config.js
@@ -1,22 +1,25 @@
-const env = 'prod' // 'dev' | 'prod'
+const env = 'dev' // 'dev' | 'prod'
 const ga4Ids = {
   dev: 'G-36HYH6NF6P',
   prod: 'G-341XFN0675',
 }
 export const ga4Id = ga4Ids[env]
 
-const slug = 'ccc_ukraine_202405'
+const slug = 'ccc_ukraine_202502'
 
 export const projectWording = {
   slug,
-  ogTitle: `旗幟下　別無選擇：俄羅斯戰俘獨家專訪/Under the Banner: No Escape`,
-  ogImage: `https://v3-statics.mirrormedia.mg/images/8ff392db-632f-4518-a219-8655178d1909-w1600.jpg`,
-  ogDescription: `| 烏俄戰爭開打2年多以來，張乾琦前後8次前往烏克蘭戰地進行攝影記錄工作。最近這次，他獲得批准，成為華文媒體首位採訪俄羅斯戰俘的記者。<br>不同於之前幾次，張乾琦隨烏軍前進，見證戰爭對這塊土地與人民帶來的殘酷、荒謬與悲涼。這一次，他將鏡頭凝視俄羅斯戰俘，透過專訪聆聽他者之聲。他的提問和觀察角度像稜鏡一般，照出這場絕望之戰中人性的卑微。例如，才22歲的俄羅斯中尉告訴張乾琦：「我只想回家。」「我只有一條路可走—加入戰爭。」/ “No one asked whether we wanted to go. We got the order, and forward we went.”<br>These words come from a captured Russian lieutenant from the 71st Motorized Infantry Regiment, interviewed in Zaporizhia. Like countless others, his story reveals war's often unwanted realities.`,
+  ogTitle: `戰與逃：自由的代價`,
+  ogImage: `https://v3-statics.mirrormedia.mg/images/666ecc28-d0dd-4c7d-a070-2e0d8ab80470-w1600.jpg`,
+  ogDescription: `你還記得二十歲時的自己嗎？你在讀書嗎？還是在工作？你有什麼夢想？
+  二十來歲的頓涅茨克居民迪馬，自2022年2月在佔領區被俄羅斯軍隊強制徵召後，用日記紀錄戰爭惡魔的面貌。短短七個月後，他在戰場上喪生。
+  烏俄戰爭滿三年了，交戰雙方粗估破百萬傷亡，多少珍貴生命成為冰冷的數字、無意義的砲灰。紀實攝影家張乾琦自開戰以來，第九次冒險深入烏克蘭戰地，他用圖、文幫我們紀錄下更多像迪馬這樣的故事。
+  奮戰的勇氣、死亡的氣味、逃兵的心聲…，64歲的攝影老兵藉著持續紀錄戰地實況，在在反思他心心念念、深愛著的台灣的處境。`,
 }
 
 export const ogUrl =
   env === 'dev'
-    ? 'https://www.mirrormedia.mg/projects/dev-ccc_ukraine_202405/index.html'
-    : 'https://www.mirrormedia.mg/projects/ccc_ukraine_202405/index.html'
+    ? 'https://www.mirrormedia.mg/projects/dev-ccc_ukraine_202502/index.html'
+    : 'https://www.mirrormedia.mg/projects/ccc_ukraine_202502/index.html'
 
 export const projectName = projectWording.slug
